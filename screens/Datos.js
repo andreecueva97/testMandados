432fc1b374e463a70edf0f6328d61644d0e4041c
andreecueva97 <andreecueva6@gmail.com>
2021-06-11 13:39:31 -0300

    update datos.js  and adding textinput and text titles for values

--- screens/Datos.js
+++ screens/Datos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
+  StyleSheet,TextInput,
   Text,View,
   useColorScheme,TouchableOpacity
 } from 'react-native';
@@ -9,25 +9,71 @@ import {
 class Datos extends React.Component{
     constructor(props){
         super(props);
-
+    }
+    state={nombre:'',apellido:'',edad:"",dni:''};
+    handleNombre=(texto)=>{
+      this.setState({nombre:texto})
+    }
+    handleNombre=(texto)=>{
+      this.setState({apellido:texto})
+    }
+    handleNombre=(texto)=>{
+      this.setState({edad:texto})
+    }
+    handleNombre=(texto)=>{
+      this.setState({dni:texto})
     }
     render(){
       return(
         <View style={styles.inicio_View}>
         <Text style ={{textAlign:'center',backgroundColor:'white',alignContent:'center',justifyContent:'center'}}>ACA DATOS</Text>
         <View style={styles.inicio_Title}>
-          <Text style={styles.inicio_Text}>Test de los Mandados</Text>
-        </View>          
-        <View style={styles.inicio_Logo}>
-          <Text style={styles.inicio_Text}>Logo</Text>
-        </View>
-        <View style={{backgroundColor:'red',alignContent: 'center', justifyContent: 'center',flexDirection: 'row', borderRadius: 3,
+              <Text style={styles.inicio_Text}>Nombre</Text>
+            </View>
+        <TextInput style={styles.datos_textinput}
+          underlineColorAndroid="transparent"
+          placeholder="nombre"
+          placeholderTextColor="blue"
+          autoCapitalize = "none"
+          onChangeText={this.handleNombre}
+        />
+        <View style={styles.inicio_Title}>
+              <Text style={styles.inicio_Text}>Apellido</Text>
+            </View>
+        <TextInput style={styles.datos_textinput}
+          underlineColorAndroid="transparent"
+          placeholder="apellido"
+          placeholderTextColor="blue"
+          autoCapitalize = "none"
+          onChangeText={this.handleApellido}
+        />
+        <View style={styles.inicio_Title}>
+              <Text style={styles.inicio_Text}>Edad</Text>
+            </View>
+        <TextInput style={styles.datos_textinput}
+          underlineColorAndroid="transparent"
+          placeholder="edad"
+          placeholderTextColor="blue"
+          autoCapitalize = "none"
+          onChangeText={this.handleApellido}
+        />
+        <View style={styles.inicio_Title}>
+              <Text style={styles.inicio_Text}>DNI</Text>
+            </View> 
+        <TextInput style={styles.datos_textinput}
+          underlineColorAndroid="transparent"
+          placeholder="dni"
+          placeholderTextColor="blue"
+          autoCapitalize = "none"
+          onChangeText={this.handleApellido}
+        />
+        <View style={{paddingTop:36,backgroundColor:'red',alignContent: 'center', justifyContent: 'center',flexDirection: 'row', borderRadius: 3,
 marginBottom: 10}}>
         <TouchableOpacity 
             style={styles.inicio_Button}
             onPress={()=>{}}
           >
-              <Text style={styles.inicio_Text}>INICIAR</Text>
+              <Text style={styles.inicio_Text}>Iniciar Juego</Text>
           </TouchableOpacity>
         </View>
         
@@ -37,33 +83,26 @@ marginBottom: 10}}>
 }
 
 const styles = StyleSheet.create({
-
+  datos_textinput:{
+    margin:15,
+    height:40,
+    borderColor:'white',
+    borderWidth:1,
+  },
   inicio_View:{
-    flexDirection:'column',backgroundColor:'blue',
+    flexDirection:'column',backgroundColor:'green',
     flex:1,
-    alignContent: 'center', justifyContent: 'center'
+    alignContent: 'center', justifyContent: 'center',
+    paddingTop:23,
   },
   inicio_Text:{
     backgroundColor:'yellow', fontSize:30,
     alignContent: 'center', justifyContent: 'center',
     borderWidth: 2,alignItems: 'center',textAlign:'center',
   },
-  inicio_Title:{
-    fontSize:30,
-    color:'yellow',
-    alignContent: 'center', justifyContent: 'center',
-    backgroundColor:'blue',textAlign:'center',
-  },
-  inicio_Logo:{
-    fontSize:30,
-    position:'relative',
-    alignContent: 'center', justifyContent: 'center',
-    textAlign:'center',
-  },
   inicio_Button:{
     fontSize:30,
     borderRadius:10,
-    
     width:300,
     backgroundColor:'grey',
     padding: 10,
